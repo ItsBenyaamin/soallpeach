01edea1979cb08f6a151263c2b8ec04cae4a9a65
graymind75 <me@benyaamin.com>
2020-04-13 15:32:11 +0430

    some changes

--- prime/src/main.rs
+++ prime/src/main.rs
@@ -1,35 +1,36 @@
 use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
-#[warn(unused)]
 fn main() {
     let args: Vec<String> = env::args().collect::<Vec<String>>();
     if args.len() < 2 {
         println!("there is no file passed!");
         return
     }
-    let file_lines = std::fs::read_to_string(&args[1]).expect("file not found!");
-    for current in file_lines.split("\n") {
-        let num: i64 = current.parse().unwrap();
-        if is_prime(num) {
-            println!("{}", 1);
-        }else {
-            println!("{}", 0);
-        }
-    }
+    let file = File::open(&args[1]).unwrap();
+    BufReader::new(file)
+        .lines()
+        .filter_map(std::io::Result::ok)
+        .for_each(|x| {
+            let num: i64 = x.parse().unwrap();
+            if is_prime(num) {
+                println!("{}", 1);
+            }else {
+                println!("{}", 0);
+            }
+        });
 }
 
 fn is_prime(number: i64) -> bool {
-    if number <= 1 {
+    if number <= 1 || number % 2 == 0 || number % 3 == 0{
         return false;
     }
     if number <=3 {
         return true;
     }
-    if number % 2 == 0 || number % 3 == 0{
-        return false;
-    }
-    let num_sqrt = (number as f64).sqrt();
-    for i in 2..num_sqrt as i64 {
+    let num_sqrt = (number as f64).sqrt() as i64;
+    for i in 2..num_sqrt {
         if number % i == 0 {
             return true;
         }
