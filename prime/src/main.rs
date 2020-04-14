ca82ec0152565edadad01beea3af307ddb8cb6ea
graymind75 <me@benyaamin.com>
2020-04-14 15:26:26 +0430

    update

--- prime/src/main.rs
+++ prime/src/main.rs
@@ -8,18 +8,20 @@ fn main() {
         println!("there is no file passed!");
         return
     }
+    let mut output = String::new();
     let file = File::open(&args[1]).unwrap();
-    BufReader::new(file)
+    BufReader::new(&file)
         .lines()
         .filter_map(std::io::Result::ok)
         .for_each(|x| {
             let num: i64 = x.parse().unwrap();
             if is_prime(num) {
-                println!("{}", 1);
+                output += "1\n";
             }else {
-                println!("{}", 0);
+                output += "0\n";
             }
         });
+    print!("{}", output);
 }
 
 fn is_prime(number: i64) -> bool {
@@ -32,8 +34,8 @@ fn is_prime(number: i64) -> bool {
     if number % 2 == 0 || number % 3 == 0{
         return false;
     }
-    let num_sqrt = (number as f64).sqrt();
-    for i in 2..num_sqrt as i64 {
+    let num_sqrt = (number as f64).sqrt() as i64 ;
+    for i in 2..num_sqrt{
         if number % i == 0 {
             return false;
         }
