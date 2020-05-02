9f7c15bb31db91e3ad96647290cb86b75d055ccd
graymind75 <me@benyaamin.com>
2020-05-02 18:36:39 +0430

    fix response problem

--- countme/src/main.rs
+++ countme/src/main.rs
@@ -6,21 +6,21 @@ use rocket::{State, Data};
 use rocket::response::status;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::io::Read;
-
+use rocket::http::Status;
 
 #[post("/", data = "<data>")]
-fn sum_route(counter: State<HitCount>, data: Data) -> status::Accepted<String> {
+fn sum_route(counter: State<'_, HitCount>, data: Data) -> Result<status::Custom<String>, String> {
     let mut str_data = String::new();
     data.open().read_to_string(&mut str_data).expect("error in reading data!");
     let int_number: usize = str_data.parse().expect("data is not a number!");
     let result = int_number + counter.count.load(Ordering::SeqCst);
     counter.count.store(result, Ordering::SeqCst);
-    status::Accepted(Some(format!("{}", result)))
+    Ok(status::Custom(Status::Ok,format!("{}", result)))
 }
 
 #[get("/count")]
-fn count_route(counter: State<HitCount>) -> status::Accepted<String> {
-    status::Accepted(Some(format!("{}", counter.count.load(Ordering::SeqCst))))
+fn count_route(counter: State<'_, HitCount>) -> Result<status::Custom<String>, String> {
+    Ok(status::Custom(Status::Ok, format!("{}", counter.count.load(Ordering::SeqCst))))
 }
 
 struct HitCount {
@@ -31,7 +31,7 @@ fn main() {
     let config = Config::build(Environment::Production)
         .address("localhost")
         .secret_key("sa4KVi6JOzGizfmXxckcUyrYXTU4IGgKFXHVrZeH050=")
-        .port(80)
+        .port(8080)
         .finalize();
     match config {
         Ok(config)=> {
